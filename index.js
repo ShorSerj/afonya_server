dd8b4ae82c2471a859b29213dbaed29655b42fdb
ShorSerj <shorser8@gmail.com>
2022-02-23 15:56:01 +0300

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const data = require('./data.json') 
 const express = require('express')
-const app = express()
+const cors = require('cors')
+const data = require('./data.json') 
 
+const app = express()
+app.use(cors())
 
 const PORT = process.env.PORT || 80
 
