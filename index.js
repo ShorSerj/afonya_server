efee216940ee44bd1fecf08fc88ee1cfe1e2c824
ShorSerj <shorser8@gmail.com>
2022-02-23 15:01:17 +0300

    create

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 80
+
+app.get('/', (req,res) => {
+    res.end('<h1>Home Page</h1>')
+})
+app.listen(PORT, () => {
+    console.log('Server has been started...')
+})
