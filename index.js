082356a2ef1bee88bebdaa3f754b8d6e5c67a45b
ShorSerj <shorser8@gmail.com>
2022-02-24 18:27:56 +0300

    add data-min

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const cors = require('cors')
-const data = require('./data.json') 
+const dataMin = require('./data-min.json') 
+const dataMax = require('./data-max.json') 
 
 const app = express()
+
 app.use(cors())
 
 const PORT = process.env.PORT || 80
 
-app.get('/', (req,res) => {
-    res.json(data)
+app.get('/data-min', (req,res) => {
+    res.json(dataMin)
+})
+
+app.get('/data-max', (req,res) => {
+    res.json(dataMax)
 })
 app.listen(PORT, () => {
     console.log('Server has been started...')
